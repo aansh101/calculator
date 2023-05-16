66ad76ef72842e784b5d936c9978bf472e97d750
sanchita bharadwaj <sanchita bharadwaj@Dell>
2023-05-16 16:22:11 +0530

    this by s

---
+++ DemoGit/src/basic_program/Calculator.java
@@ -0,0 +1,16 @@
+package basic_program;
+
+public class Calculator {
+	
+
+	public static void main(String[] args) {
+		int a=20;
+		int b=10;
+		int c;	
+		c=a+b;
+		
+		System.out.println(c);
+
+	}
+
+}
